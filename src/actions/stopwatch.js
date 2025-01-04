09e6c5f6d792324ce8148239cf406ba1c219c90c
MSoleimanzadeh <mina.soleimanzadeh@gmail.com>
2025-01-04 07:41:43 +0330

    Add actions

---
+++ src/actions/stopwatch.js
@@ -0,0 +1,19 @@
+export const start = () => ({
+  type: 'START'
+});
+
+export const puse = () => ({
+  type: 'PAUSE'
+});
+
+export const resume = () => ({
+  type: 'RESUME'
+});
+
+export const reset = () => ({
+  type: 'RESET'
+});
+
+export const lap = () => ({
+  type: 'LAP'
+});
